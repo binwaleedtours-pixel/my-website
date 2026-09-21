ca4a38cc37feaf1a6f413d173a79845d1f703a18
waleedtours <binwaleedtours@gmail.com>
2026-09-21 11:01:17 +0500

    hero image remove

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import "./Hero.css";
-import banner1 from "./assets/IMG_1276.jpg";
-import banner2 from "./assets/IMG_1162.jpg";
-import banner3 from "./assets/IMG_1420.jpg";
+
 const slidesData = [
   {
     id: 1,
@@ -11,7 +9,7 @@ const slidesData = [
     description:
       "Embark on unforgettable journeys through Pakistan's most serene valleys, Lakes and rugged peaks.",
     bgImage:
-      banner1,
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1920&q=80",
   },
   {
     id: 2,
@@ -19,7 +17,7 @@ const slidesData = [
     description:
       "Travel through majestic mountains, scenic valleys, and hidden gems while creating memories with a family environment  that last a lifetime.",
     bgImage:
-      banner2,
+      "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&w=1920&q=80",
   },
   {
     id: 3,
@@ -27,7 +25,7 @@ const slidesData = [
     description:
       "Discover breathtaking destinations, unforgettable adventures, and thoughtfully planned journeys with a registered tourism company.",
     bgImage:
-      banner3,
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1920&q=80",
   },
 ];
 
