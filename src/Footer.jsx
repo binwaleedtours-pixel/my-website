5b4a5c41b7cbce1551b0f296b96e409201d1b397
waleedtours <binwaleedtours@gmail.com>
2026-09-21 18:09:40 +0500

    link updated

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -25,7 +25,7 @@ function Footer() {
           <ul className="footer-links">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/tours">Our Tours</Link></li>
-            <li><Link to="/destinations">Gallery</Link></li>
+            <li><Link to="/GalleryPage">Gallery</Link></li>
             <li><Link to="/aboutPage">About Us</Link></li>
           </ul>
         </div>
